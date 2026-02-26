20dcf96fdcbc2c70ddcc43626ebcf9cb2b5babfa
akslask <vasyok@vasyok.com>
2026-02-25 19:36:29 -0800

    21 и немного 22

--- resources/views/blade.blade.php
+++ resources/views/blade.blade.php
@@ -28,70 +28,79 @@
     @endforeach
 
     @forelse ($city as $elem)
-    <p>{{ $elem }}</p>
-    @empty
-    <p>Москва</p>
-    @endforelse
-
-    @foreach ($location as $elem)
-        <p>{{ $elem }}</p>
-    @forelse ($city as $elem)
         <p>{{ $elem }}</p>
     @empty
         <p>Москва</p>
     @endforelse
-    @forelse ($country as $elem)
+
+    @foreach ($location as $elem)
         <p>{{ $elem }}</p>
-    @empty
-        <p>Россия</p>
-    @endforelse  
+        @forelse ($city as $elem)
+            <p>{{ $elem }}</p>
+        @empty
+            <p>Москва</p>
+        @endforelse
+        @forelse ($country as $elem)
+            <p>{{ $elem }}</p>
+        @empty
+            <p>Россия</p>
+        @endforelse
     @endforeach
 
     @if ($year > 0)
-    <p>Year: {{ $year }}</p>
+        <p>Year: {{ $year }}</p>
     @else
-    <p>Year: {{ date('Y') }}</p>
+        <p>Year: {{ date('Y') }}</p>
     @endif
 
     @if($month > 0)
-    <p>Month: {{ $month }}</p>
+        <p>Month: {{ $month }}</p>
     @else
-    <p>Month: {{ date('m') }}</p>
+        <p>Month: {{ date('m') }}</p>
     @endif
 
     @if ($day > 0)
-    <p>Day: {{ $elem }}</p>
+        <p>Day: {{ $elem }}</p>
     @else
-    <p>Day: {{ date('d') }}</p>
+        <p>Day: {{ date('d') }}</p>
     @endif
 
     <p>{!! $str !!}</p> {{-- комментарий --}}
 
     @if ($userAge > 18)
-    <p>Текст для пользователя старше 18 лет. Пользователь совершеннолетний</p>
+        <p>Текст для пользователя старше 18 лет. Пользователь совершеннолетний</p>
     @elseif ($userAge == 18)
-    <p>Текст для 18-летнего пользователя. Пользователь совершеннолетний</p>
+        <p>Текст для 18-летнего пользователя. Пользователь совершеннолетний</p>
     @else
-    <p>Текст для пользователя младше 18. Пользователь несовершеннолетний</p>
+        <p>Текст для пользователя младше 18. Пользователь несовершеннолетний</p>
     @endif
-    
+
     @unless ($userAge >= 18)
-    <p>Пользователь несовершеннолетний</p>
+        <p>Пользователь несовершеннолетний</p>
     @endunless
 
-    {{ $sum = 0 }}
     @foreach ($numbers as $elem)
-    <ul>
-        <li>{{ $elem * $elem}}</li>
-        <li>{{ sqrt($elem) }}</li>
-    </ul>
-    @if ($loop->count > 0)
-    {{ $sum += $elem }}
-    @else
-    <p>Массив пустой</p>
-    @endif
+        <ul>
+            <li><b>{{ $elem }}</b></li>
+            <li>{{ $elem * $elem}}</li>
+            <li>{{ sqrt($elem) }}</li>
+        </ul>
+    @endforeach
+
+    @foreach ($numbers as $elem)
+        @if ($loop->count > 0)
+        @php
+            $sum += $elem
+        @endphp
+        @else
+            <p>Массив пустой</p>
+        @endif
     @endforeach
     <p>{{ $sum }}</p>
+
+    <ul>
+    @foreach ($strokes as $str)
+    <li>{{ $str['first'] }}</li>
 </body>
 
 </html>
