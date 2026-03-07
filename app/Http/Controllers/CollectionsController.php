bc525a74aafce4c6f7cfd3067f68b1bac823e22f
akslask <vasyok@vasyok.com>
2026-03-06 23:30:11 -0800

    Создание и запуск миграции

--- app/Http/Controllers/CollectionsController.php
+++ app/Http/Controllers/CollectionsController.php
@@ -17,15 +17,5 @@ class CollectionsController extends Controller
                 return Str::upper($value);
             });
         });
-
-        $average = collect([
-            ['foo' => 10],
-            ['foo' => 10],
-            ['foo' => 20],
-            ['foo' => 40]
-        ])->avg('foo');
-
-        $average = collect([1, 1, 2, 4])->avg();
-        dd($average);
     }
 }
