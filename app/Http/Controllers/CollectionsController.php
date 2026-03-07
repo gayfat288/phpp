fc4e5b450d42381b45482c045479b6329117e22f
akslask <vasyok@vasyok.com>
2026-03-06 23:11:39 -0800

    average

--- app/Http/Controllers/CollectionsController.php
+++ app/Http/Controllers/CollectionsController.php
@@ -18,8 +18,14 @@ class CollectionsController extends Controller
             });
         });
 
-        //Метод all
-        $all = collect([1, 2, 3])->all();
-        dd($all);
+        $average = collect([
+            ['foo' => 10],
+            ['foo' => 10],
+            ['foo' => 20],
+            ['foo' => 40]
+        ])->avg('foo');
+
+        $average = collect([1, 1, 2, 4])->avg();
+        dd($average);
     }
 }
