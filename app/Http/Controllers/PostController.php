b055c7966d2bc79b1f7fe6e40c86f9389c865975
gayfat288 <gdheu7651@gmail.com>
2026-03-19 02:33:38 +0600

    27

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function show()
     {
-        $users = DB::table('users')->whereIn('id', [1, 2, 3, 5]);
+        $users = DB::table('users')->whereNotIn('id', [1, 2, 3, 5]);
         return view('users', ['users' => $id]);
     }
 }
