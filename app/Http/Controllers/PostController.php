f2dfa745b3c184cd9ad6b3754657f42f0dbfd824
gayfat288 <gdheu7651@gmail.com>
2026-03-19 02:40:16 +0600

    37

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function show()
     {
-        $users = DB::table('users')->orderBy('created_at', 'asc')->get();
+        $users = DB::table('users')->where('age' '>', 30)->orderBy('created_at', 'asc')->get();
         return view('users', ['users' => $user]);
     }
 }
