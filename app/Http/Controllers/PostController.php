0d8f463ca19a788b9d09a24b3bcb1da62111be77
gayfat288 <gdheu7651@gmail.com>
2026-03-19 02:54:23 +0600

    49

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,10 +10,20 @@ class PostController extends Controller
     public function show()
     {
         $users = DB::table('users')->insert([
-            'name'=>'arthur',
-            'age'=>40,
-            'salary'=>700,
-            'email'=>'arthur@mail.com'
+            ['name'=>'miles',
+            'age'=>27,
+            'salary'=>500,
+            'email'=>'miles@mail.com'],
+
+            ['name'=>'owen',
+            'age'=>24,
+            'salary'=>600,
+            'email'=>'owen@mail.com'],
+
+            ['name'=>'leo',
+            'age'=>30,
+            'salary'=>1000,
+            'email'=>'leo@mail.com']
         ]);
         dump($users);
         return view('users', ['users' => $user]);
