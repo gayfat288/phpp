635542c066e85e316044b6376b4f542b51556da3
gayfat288 <gdheu7651@gmail.com>
2026-03-19 02:58:55 +0600

    54

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function show()
     {
-        $users = DB::table('users')->where('age', '=', 30)->increment('salary', 100);
+        $users = DB::table('users')->where('id', '=', 5)->delete();
         dump($users);
         return view('users', ['users' => $user]);
     }
