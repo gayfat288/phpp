52f170ded4b9fae7bb3fe61d0a87be2b00fe7fc1
gayfat288 <gdheu7651@gmail.com>
2026-03-19 02:01:26 +0600

    Дамп
    Дамп

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,6 @@ class PostController extends Controller
 {
     public function show()
     {
-        $query = DB::table('posts')->where('id', '!=', 3)->toSql();
-        dump($query);
+        DB::table('posts')->where('id', '!=', 3)->dd();
     }
 }
