e7473d405c0e7104fc3ddd1298c6b8d8acd1b483
akslask <vasyok@vasyok.com>
2026-03-31 03:24:59 -0700

    28.2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,75 +8,8 @@ use App\Post;
 
 class PostController extends Controller
 {
-    use SoftDeletes;
-
-    protected $dates = ['deleted_at'];
-
-    public function soft() {
-        Schema::table('posts', function ($table) {
-        $table->softdeletes();
-        });
-    }   
-
-    public function getAll($order = 'date', $dir ='desc')
+    public function usersProfiles()
     {
-        $posts = Post::orderBy($order, $dir)->all();
-        return view('post', ['posts'=>$posts]);
-    }
-
-    public function getOne($id)
-    {
-        $postId = Post::findOrFail($id);
-        return view('postId', ['postId'=>$post]);
-    }
-
-    public function newPost(Request $request, $id)
-    {
-        $postt = Post::find($id);
-
-        if ($request->has('submit')) {
-            $postt->title = $request->title;
-            $postt->desc = $request->desc;
-            $postt->date = $request->date;
-            $postt->text = $request->text;
-
-            $post->save();
-
-            return redirect()->to('post/all')->with('upd', 'id'=>$request->id, 'title'=>$request->title);
-        }
-        return view('test.newPost', ['postt' => $postt]);
-    }
-
-    public function updatedPost()
-    {
-        $post = Post::find(1);
-        $post->title = 'new title';
-        $post->desc = 'new description';
-
-        $post->save();
-    }
-
-    public function delPost(Request $requestt, $id)
-    {
-        $delPost = Post::find($id);
-
-        if($requestt->has('submit')) {
-            $delPost->delete();
-
-            return redirect()->to('post/all')->with('del', 'title'=>$request->title)->with('success', 'Статья удалена');
-        }
-
-        return view('delPost', ['delPost' => $delete]);
-    }
-
-    public function getDeletedPost()
-    {
-        if($post->trashed()) {
-            return view('post', ['post' => $deleted]);
-        }
-    }
-
-    public function restorePost() {
-
+        $profile = Users::find(1)->profile;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,4 @@ use App\Http\Controllers\BladeController;
 use App\Http\Controllers\PostController;
 
 Route::get('/blade', [BladeController::class, 'show']);
-Route::match(['get', 'post'], '/post/new/{id}', 'PostController@newPost');
-Route::get('/post/upd', [PostController::class, 'updatedPost']);
-Route::get('/post/del/{id}', [PostController::class, 'delPost']);
-Route::get('/post/deleted', [PostController::class, 'getDeletedPost']);
+Route::get('/profiles', [PostController::class, 'usersProfiles']);
