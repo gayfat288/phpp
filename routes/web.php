8704d44f14247b11ac996306b8738a5835852ec2
akslask <vasyok@vasyok.com>
2026-03-31 03:20:25 -0700

    28.1

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@ use App\Http\Controllers\BladeController;
 use App\Http\Controllers\PostController;
 
 Route::get('/blade', [BladeController::class, 'show']);
-Route::get('/post/all/{order?}/{dir?}', [PostController::class, 'getAll'])->where($order, '=', 'id|date|title')->where($dir, '=', 'asc|desc');
-Route::get('/post/{id}', [PostController::class, 'getOne'])->where($id, '=', '[0-9]+');
 Route::match(['get', 'post'], '/post/new/{id}', 'PostController@newPost');
 Route::get('/post/upd', [PostController::class, 'updatedPost']);
 Route::get('/post/del/{id}', [PostController::class, 'delPost']);
