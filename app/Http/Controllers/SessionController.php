4afc0340eb9dc392025e0abed62d0f1e7f9e5edb
akslask <vasyok@vasyok.com>
2026-04-16 22:30:40 -0700

    14.9

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class SessionController extends Controller
 {
     public function show(Request $request) {
-        $request->session()->flush();
+        $request->session()->put('a', '1');
+        $request->session()->put('b', '2');
+        $request->session()->put('c', '3');
+        $request->session()->put('d', '4');
+
+        $data = $request->session()->all();
+        var_dump($data);
         return view('session.show');
     }
 }
