4ac9342cdaa68731c4f05b068d198aafde780a96
akslask <vasyok@vasyok.com>
2026-04-16 21:08:49 -0700

    13.1

--- resources/views/form/show.blade.php
+++ resources/views/form/show.blade.php
@@ -1,7 +1,7 @@
 <form action="/form/result/" method="GET">
-    <input type="text" name="text">
-    <input type="text" name="text">
-    <input type="text" name="text">
-    <input type="text" name="text">
+    <input type="text" name="text1">
+    <input type="number" name="num2">
+    <input type="text" name="text3">
+    <input type="text" name="text4">
     <input type="submit">
 </form>
