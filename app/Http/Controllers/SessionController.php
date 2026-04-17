8187cfbc9e5c3c2e4794387e5108e4fd059f176d
akslask <vasyok@vasyok.com>
2026-04-16 22:20:43 -0700

    14.6

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -8,11 +8,8 @@ class SessionController extends Controller
 {
     public function show(Request $request) {
         $request->session()->put('arr', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-        $arr = $request->session()->get('arr');
-        var_dump($arr);
-        $request->session()->push('arr', 19);
-        $arr_upd = $request->session()->get('arr');
-        var_dump($arr_upd);
+        $request->session()->put('arr2', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        $request->session()->forget('arr');
         return view ('session.show');
     }
 }
