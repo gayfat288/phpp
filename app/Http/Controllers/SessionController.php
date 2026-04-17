a6c58453160d881229334bc068e50e1d117d44ff
akslask <vasyok@vasyok.com>
2026-04-16 22:14:42 -0700

    14.5

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 class SessionController extends Controller
 {
     public function show(Request $request) {
-        $current_time = date('H:i:s');
-        $time = $request->session()->get('time', $current_time);
-        echo $time;
-        $request->session()->put('time', $current_time);
-        return view('session.show');
+        $request->session()->put('arr', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        $arr = $request->session()->get('arr');
+        var_dump($arr);
+        $request->session()->push('arr', 19);
+        $arr_upd = $request->session()->get('arr');
+        var_dump($arr_upd);
+        return view ('session.show');
     }
 }
