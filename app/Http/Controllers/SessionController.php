d60862b161232c712baef5869829799300b300a5
akslask <vasyok@vasyok.com>
2026-04-16 22:26:25 -0700

    14.8

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,13 +6,8 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    public function put(Request $request) {
-        $value = $request->session()->put('key', 1);
+    public function show(Request $request) {
+        $request->session()->flush();
         return view('session.show');
-    }
-
-    public function get(Request $request) {
-        $val = $request->session()->pull('key');
-        return view('session.get', ['val' => $val]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ Route::get('/', function () {
 
 use App\Http\Controllers\SessionController;
 
-Route::get('/session/get', [SessionController::class, 'get']);
+Route::get('/session', [SessionController::class, 'show']);
