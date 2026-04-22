0831d0138d6ad321f90a194230d8f659224547d0
akslask <vasyok@vasyok.com>
2026-04-21 21:07:25 -0700

    14.11 - 14.12

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,15 +7,8 @@ use Illuminate\Http\Request;
 class SessionController extends Controller
 {
     public function show(Request $request) {
-        $request->session()->put('test', 'test');
-
-        if ($request->session()->has('test')) {
-            $val = $request->session()->get('test');
-            echo($val);
-        }
-        else {
-            $request->session()->put('test', 'test');
-        }
-        return view('session.show');
+        session(['test'=>'test1']);
+        $value = session('test');
+        return view('session.show', ['value'=>$value]);
     }
 }
