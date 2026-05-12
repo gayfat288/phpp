c7f4eebb28ce47631f1791dc45f8941a2e7d4551
akslask <vasyok@vasyok.com>
2026-05-12 01:26:09 -0700

    модель для таблицы posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,6 @@ class PostController extends Controller
 {
     public function show()
     {
-        //создать миграции! в файле есть!!!(в папке с контроллерами)
+        
     }
 }
--- resources/views/form/show.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="" method="POST">
-    {{ csrf_field() }}
-    <input type="number" name="num1">
-    <input type="number" name="num2">
-    <input type="submit">
-</form>
