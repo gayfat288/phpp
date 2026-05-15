ab4fb99230cf3029752dcaa52c4d1b175c3d39df
akslask <vasyok@vasyok.com>
2026-05-14 21:15:57 -0700

    25.9

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,9 @@ use App\Models\Posts;
 
 class PostController extends Controller
 {
-    public function getAll($order='date')
+    public function getAll($order='date', $dir='desc')
     {
-       $posts = Posts::orderBy($order, 'desc')->get();
+       $posts = Posts::orderBy($order, $dir)->get();
        return view('posts.show', ['posts'=>$posts]);
     }
 
